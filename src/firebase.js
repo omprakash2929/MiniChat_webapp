b2ea27ac00832787c3d159bb8cc64b6d226d379c
Chauhan Omprakash <91268538+omprakash2929@users.noreply.github.com>
2022-11-22 17:09:42 +0530

    fixtt

--- src/firebase.js
+++ src/firebase.js
@@ -5,24 +5,10 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBPN5BumVOdNX7JNu8PLXvXQol6-Qu3g9A",
-//   authDomain: "minichat-47712.firebaseapp.com",
-//   projectId: "minichat-47712",
-//   storageBucket: "minichat-47712.appspot.com",
-//   messagingSenderId: "1000180505183",
-//   appId: "1:1000180505183:web:bf61b3d1c469e54ea1d9ea",
-//   measurementId: "G-J5Y6QWBDVP"
-// };
-const firebaseConfig = {
-  apiKey: "AIzaSyBFvpyBv7A2_4L9ToKQjZpGxzJ0nQ7GB8U",
-  authDomain: "webchat-406c1.firebaseapp.com",
-  projectId: "webchat-406c1",
-  storageBucket: "webchat-406c1.appspot.com",
-  messagingSenderId: "1011856920611",
-  appId: "1:1011856920611:web:e153fe6b762314fa8ba196"
-};
+// Your web app's Firebase configuration
 
 
 
